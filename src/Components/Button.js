428ba8b09c8e4f84196c761836e74b8da4bb4647
unknown <larsroovers@gmail.com>
2020-07-08 16:20:39 +0200

    added a11y accessibility addon

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Button = ({buttonText}) => {
     return (
     <button>{buttonText}</button>
     )
+}
+
+Button.propTypes = {
+    buttonText: PropTypes.string
 }
 
 export default Button;
