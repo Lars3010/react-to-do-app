2268594fdc0a5181d23579b0a086147f7bd122dc
unknown <larsroovers@gmail.com>
2020-07-09 00:27:34 +0200

    added viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
 	stories: ['../src/components/**/*.stories.js'],
     addons: [
-    	'@storybook/addon-a11y',
+		'@storybook/addon-a11y',
+		'@storybook/addon-viewport/register'
   	],
 };
